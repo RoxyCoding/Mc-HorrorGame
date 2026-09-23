e67fd43328eaf9a74d17b261e0f601ff28bd5e91
Chihalu <m.chihiro0314@gmail.com>
2026-09-24 02:57:18 +0900

    first commit

---
+++ src/client/java/chihalu/horror/client/HorrorDataGenerator.java
@@ -0,0 +1,11 @@
+package chihalu.horror.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HorrorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
